8ea0b4fd347c92bb5c70923e132d689501b94a68
Mihaela Codrescu <mihaela_cod@yahoo.com>
2022-10-06 19:57:30 +0300

    extract getTeamHTML function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+function getTeamHTML(team) {
+  return `
+  <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td>
+      <button class="btn-update" >Update</button>
+      <button class="btn-delete" >Delete</button>
+    </td>
+  </tr>`;
+}
+
 function displayTeams(teams) {
-  // var teansHTML = "";
+  // var teamsHTML = "";
   // for (var i = 0; i < teams.length; i++) {
-  //   teamsHTML += "<tr><td>TODU</td></tr>";
+  //   teamsHTML += getTeamHTML(teams[i]);
   // }
 
   // transforma in HTML
@@ -9,19 +25,7 @@ function displayTeams(teams) {
   var teamsHTML = "";
   teams.forEach(function (team) {
     // console.info(team.promotion);
-    teamsHTML += `
-      <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>
-          <a href="${team.url}">open</a>
-        </td>
-        <td>
-          <button class="btn-update" >Update</button>
-          <button class="btn-delete" >Delete</button>
-        </td>
-      </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
 
   // afisare
